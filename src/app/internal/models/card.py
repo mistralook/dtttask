6c72b53ba0908656a671560d79f7b3f92c9f7cbf
mistralook <mistralook@gmail.com>
2022-04-19 19:32:28 +0500

    aboba

--- src/app/internal/models/card.py
+++ src/app/internal/models/card.py
@@ -8,7 +8,7 @@ from .user import User
 class Card(models.Model):
     card_number = models.BigIntegerField(primary_key=True)
     owner_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column="owner_id")
-    balance = MoneyField(max_digits=14, decimal_places=2, null=True, default_currency=None)
+    balance = models.DecimalField(max_digits=19, decimal_places=2, null=True)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
     def __str__(self):
