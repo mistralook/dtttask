db19869ac41f57060ba02b5c8b20c5c1664d3f65
mistralook <mistralook@gmail.com>
2022-04-19 22:06:50 +0500

    отвечает бармен и вышвыривает ее за дверь.

--- src/app/internal/services/bank_services.py
+++ src/app/internal/services/bank_services.py
@@ -2,6 +2,7 @@ import decimal
 
 from app.internal.models.card import Card
 from app.internal.models.user import User
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.db import transaction
 
@@ -20,6 +21,14 @@ def transfer_operation(from_card, to_card, amount):
            f"From: {from_card.card_number} ({from_card.owner_id}) \n" \
            f"To: {to_card.card_number} ({to_card.owner_id}) \n" \
            f"{amount} were sent"
+
+
+def get_card_by_card_number(card_number):
+    try:
+        card = Card.objects.get(card_number=card_number)
+        return card
+    except ObjectDoesNotExist:
+        return None
 
 
 def get_balance_by_card(t_id, card_number):
@@ -42,16 +51,23 @@ def get_balance_by_account(t_id, account_number):
 
 def transfer_money_to_card(source, destination, amount):
     source_card = int(source)
-    source_card = Card.objects.get(card_number=source_card)
+    source_card = get_card_by_card_number(source_card)
+    if source_card is None:
+        return "Invalid source. Card does not exist"
     try:
         destination = int(destination)  # card
-        receiver_card = Card.objects.get(card_number=destination)
+        receiver_card = get_card_by_card_number(destination)
+        if receiver_card is None:
+            return "Invalid receiver. Card does not exist"
         operation = transfer_operation(source_card, receiver_card, amount)
     except ValueError:  # login
         if "@" in destination:
             destination = destination[1:]
-        user_id = User.objects.get(username=destination).id
-        receiver_card = Card.objects.filter(owner_id=user_id).first() # Card not exist, user not exist
+        user_id = get_user_via_username(destination)
+        if user_id is None:
+            return "Invalid receiver. User does not exist."
+        receiver_card = Card.objects.filter(owner_id=user_id).first()  # Card not exist, user not exist
+        if receiver_card is None:
+            return "Invalid receiver. User does not have cards."
         operation = transfer_operation(source_card, receiver_card, amount)
     return f"{operation}"
-
